ead1dc8a63f81280f757e28b2b9105ac0466ec2f
Brock Williams <bwill210@fiu.edu>
2023-11-09 17:32:08 -0800

    game fully functional. Added functions to check for wins and ties and created displayController module to display game messages

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const displayController = (() => {
+
+    const messageBox = document.querySelector('#message');
+    
+    const renderMessage = (message) => {
+        messageBox.innerHTML = message;
+    }
+
+    return {renderMessage};
+})();
+
 const Gameboard = (() => {
     let gameboard = ["", "", "", "", "", "", "", "", ""];
     
@@ -19,6 +30,8 @@ const Gameboard = (() => {
     }
 
     const getGameboard = () => gameboard;
+
+
 
         return {render, update, getGameboard};
 })(); 
@@ -45,24 +58,66 @@ const Game = (() => {
     }
 
     const restart = () => {
+        gameOver = false;
         for (let i = 0; i < 9; i++) {
             Gameboard.update(i, "");
         }
         Gameboard.render();
+        displayController.renderMessage("");
     };
 
+    const checkForWin = (board) => {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+        for (let i = 0; i < winningCombinations.length; i++) {
+            const [a, b, c] = winningCombinations[i];
+            if (board[a] && board[a] === board[b] && board[b] === board[c]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const checkForTie = (board) => {
+        return board.every(cell => cell !== '');
+    }
+
     const handleClick = (event) => {
+        //if game over disallow click events
+        if (gameOver){
+            return;
+        }
+
+        //logic to check if square is marked already
         let index = parseInt(event.target.id.split("-")[1]);
         if (Gameboard.getGameboard()[index] !== "")
-        return;
+            return;
 
         Gameboard.update(index, players[currentPlayerIndex].marker);
+
+        //check for 3 in a row
+        if (checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].marker)) {
+            gameOver = true;
+            displayController.renderMessage(`${players[currentPlayerIndex].name} won!`);
+        }
+        else if (checkForTie(Gameboard.getGameboard())){
+            gameOver = true;
+            displayController.renderMessage(`Tie Game!`);
+        }
+        //change players' turn
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     }
 
     return {start, restart, handleClick}; 
 })();
-
 
 
 const startButton = document.querySelector('#start-button');
